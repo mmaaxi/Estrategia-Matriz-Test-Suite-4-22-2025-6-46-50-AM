0eb299caf227c191faaacba4d70cbdd62cc6b46c
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-22 00:47:46 -0600

    agregando src/test/java/pages/tc2Page.java

---
+++ src/test/java/pages/tc2Page.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+
+public class tc2Page {
+
+    WebDriver driver;
+
+    public tc2Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void realizarAccion2() {
+        // Implementación de los pasos necesarios para realizar Acción 2
+    }
+
+    public boolean verificarResultado2() {
+        // Implementar verificación del Resultado 2
+        // Retornar true si el resultado esperado es correcto, de lo contrario false
+        return true;
+    }
+}
